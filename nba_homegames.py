0be893fa96db697ec0dbc02413cb68cf964095aa
Neal Barrina <peternealbarrina@gmail.com>
2020-03-21 23:15:14 -0700

    Added database model for Posts and Users, added comment in home.html file

--- nba_homegames.py
+++ nba_homegames.py
@@ -1,8 +1,37 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'fac97d332b5b472f5f509009eba1b419'
+app.config['SQLALCHEMY_DATABASE_UR'] = 'sqlite:///site.db'  # relative path from this file
+db = SQLAlchemy(app)  #SQLAlchemy database instance
+
+# User class for table in database
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), unique = True, nullable =  False)
+    email = db.Column(db.String(120), unique = True, nullable =  False)
+    image_file = db.Column(db.String(20), nullable =  False, default = 'default.jpg')
+    password = db.Column(db.String(60), nullable = False)  # 60 character because of hashing algorithm
+    posts = db.relationship('Post', backref = 'author', lazy = True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+# Post class for table in database
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), nullable = False)
+    date_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    content = db.Column(db.Text, nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+
 
 teams = [
     {
