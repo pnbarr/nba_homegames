c499af031b65cf2de9102df47a71c8ecf6d9618d
Neal Barrina <peternealbarrina@gmail.com>
2020-03-26 18:48:08 -0700

    - in forms.py added import for FileField and FileAllowed from flask_wtf.file - added jpg and png as files allowed for picture field - added save_picture() function in routes.py to save pictures - imported secrets and os modules in routes.py file - hash name of user uploaded pictures - store uploaded pictures in static folder - pip installed Pillow package for resizing images

--- nba_homegames/forms.py
+++ nba_homegames/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from nba_homegames.models import User
@@ -43,6 +44,7 @@ class UpdateAccountForm(FlaskForm):
                             validators = [DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                         validators = [DataRequired(),Email()])
+    picture = FileField('Update Profile Picture', validators = [FileAllowed(['jpg', 'png'])])
     submit =  SubmitField('Update')
 
     def validate_username(self, username):
